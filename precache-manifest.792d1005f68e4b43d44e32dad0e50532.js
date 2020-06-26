b0cad7c6d38aff11e2b07f2b7186411133c3fc07
Jonathan Andrés Pardo <andres.961221@gmail.com>
2020-06-25 23:10:18 -0500

    Updates

---
+++ precache-manifest.792d1005f68e4b43d44e32dad0e50532.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4402c54f21fdcb82947bf4b51562b25e",
+    "url": "/jonathandrespardo/index.html"
+  },
+  {
+    "revision": "ed61558d7910a4d7e10a",
+    "url": "/jonathandrespardo/static/css/2.2730ce90.chunk.css"
+  },
+  {
+    "revision": "369531708f6952e06821",
+    "url": "/jonathandrespardo/static/css/main.45ec9431.chunk.css"
+  },
+  {
+    "revision": "ed61558d7910a4d7e10a",
+    "url": "/jonathandrespardo/static/js/2.5737decf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jonathandrespardo/static/js/2.5737decf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369531708f6952e06821",
+    "url": "/jonathandrespardo/static/js/main.a5ea146f.chunk.js"
+  },
+  {
+    "revision": "66c2ca2b2db2f33747f9",
+    "url": "/jonathandrespardo/static/js/runtime-main.94287de5.js"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/jonathandrespardo/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/jonathandrespardo/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/jonathandrespardo/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/jonathandrespardo/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/jonathandrespardo/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/jonathandrespardo/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/jonathandrespardo/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/jonathandrespardo/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/jonathandrespardo/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/jonathandrespardo/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/jonathandrespardo/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/jonathandrespardo/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/jonathandrespardo/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/jonathandrespardo/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/jonathandrespardo/static/media/fa-solid-900.f3a7d3b5.woff"
+  }
+]);
